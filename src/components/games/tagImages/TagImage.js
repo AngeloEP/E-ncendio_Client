2b070cda94411f2f0987fb26379821cee8449ecf
Angelo19987 <angelocristobalep@gmail.com>
2021-01-26 22:26:03 -0300

    Improving image tagger design

--- src/components/games/tagImages/TagImage.js
+++ src/components/games/tagImages/TagImage.js
@@ -1,8 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Col, Container, Image, Row, Button } from 'react-bootstrap';
+import Typography from '@material-ui/core/Typography';
+import { useStyles } from './tagImageStyles';
+import Checkbox from '@material-ui/core/Checkbox';
+import incendios from '../../../assets/img/incendios.jpg';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const TagImage = () => {
+
+const TagImage = ({ history }) => {
+    const classes = useStyles()
+    const [ checked, setChecked ] = useState({
+        prevencion: false,
+        mitigacion: false,
+        riesgo: false,
+        combate: false,
+        impacto: false,
+        recuperacion: false,
+        amenaza: false,
+
+        selected: null
+    })
+    
+    const onCheck = (name, val) => {
+        const checkboxes = checked;
+        console.log(checkboxes)
+        for (let key in checkboxes) {
+            checkboxes[key] = false;
+        }
+        checkboxes[name] = true;
+        checkboxes.selected = val;
+        setChecked({ ...checked, checkboxes });
+        
+    }
+
+    const Checksito = ({ state, label, name, disable }) => {
+        return (
+        <FormControlLabel
+            
+            control={
+              <Checkbox
+                checked={state}
+                value={name}
+                onChange={(e) => onCheck(name, e.target.value)}
+                color="secondary"
+              />
+            }
+            label= {label}
+          />
+        )
+    }
+    
+    const categories = [
+        <Row  >
+            <Col xs={6} > <Checksito state={checked.prevencion} label='Prevención' name='prevencion'  /> </Col> 
+            <Col xs={6} > <Checksito state={checked.mitigacion} label='Mitigación' name='mitigacion'  /> </Col> 
+        </Row>,
+        <Row>
+            <Col xs={6} > <Checksito state={checked.riesgo} label='Riesgo' name='riesgo' /> </Col> 
+            <Col xs={6} > <Checksito state={checked.combate} label='Combate' name='combate' /> </Col>
+        </Row>,
+        <Row>
+            <Col xs={6} > <Checksito state={checked.impacto} label='Impacto' name='impacto' /> </Col> 
+            <Col xs={6} > <Checksito state={checked.recuperacion} label='Recuperación' name='recuperacion' /> </Col> 
+        </Row>,
+        <Row>
+            <Col> <Checksito state={checked.amenaza} label='Amenaza' name='amenaza' /> </Col> 
+        </Row>,
+    ]
+
     return (
-        <h2> Etiqueta imágenes </h2>
+        <Container fluid >
+            <div className={classes.topCenter} >
+                <Row>
+                    <Col>
+                    <Typography variant="h4" >
+                        Nivel 1
+                    </Typography>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className={classes.center} >
+                <Row >
+                    <Col className={classes.image} > 
+                        <Image  src={incendios} rounded/>
+                    </Col>
+                    <Col  className={classes.categorias} >
+                        <Typography variant="h6" className="mr-3" >
+                            Seleccionar Categoría
+                        </Typography>
+                        <div className="container-xs" >
+                            
+
+                            {categories}
+                            
+                        </div>
+                    </Col>
+
+                </Row>
+            </div  >
+
+            <div className={classes.bottomCenter} >
+                <Row>
+                    <Col >
+                        <Button variant="secondary"> Anterior </Button>{' '}
+                    </Col>
+
+                    <Col>
+                        <Button variant="success"> Siguiente </Button>{' '}
+                    </Col>
+                </Row>
+            </div>
+        </Container>
     );
 }
  
