1e6d2c91e03163e334d9b70b42774c23454204d9
Angelo19987 <angelocristobalep@gmail.com>
2021-11-10 00:54:38 -0300

    Add responsive styles in Profile view

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -140,7 +140,7 @@ const Profile = ( props ) => {
                                 </h6>
                             </div>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-4 divEditBtn">
                             <button
                                 className={
                                     perfil.league_id.league === "Bronce"
@@ -172,18 +172,26 @@ const Profile = ( props ) => {
                     <div className="row">
                         <div className="col-md-4">
                             <div className="profile-work">
-                                <p>Imágenes</p>
-                                <div href="!#">Etiquetadas: {perfil.imageTagCount} </div>
-                                <div href="!#">Subidas: {perfil.uploadImageCount} </div>
-                                <p>Palabras</p>
-                                <div href="!#">Etiquetadas: {perfil.wordTagCount} </div>
-                                <div href="!#">Subidas: {perfil.uploadWordCount} </div>
-                                <p>Ahorcados</p>
-                                <div href="!#">Completados: {perfil.hangmanTagCount} </div>
-                                <div href="!#">Subidos: {perfil.uploadHangmanCount} </div>
-                                <p>Selecciones Únicas</p>
-                                <div href="!#">Completadas: {perfil.uniqueSelectionTagCount} </div>
-                                <div href="!#">Subidas: {perfil.uploadUniqueSelectionCount} </div>
+                                <div>
+                                    <p>Imágenes</p>
+                                    <div href="!#">Etiquetadas: {perfil.imageTagCount} </div>
+                                    <div href="!#">Subidas: {perfil.uploadImageCount} </div>
+                                </div>
+                                <div>
+                                    <p>Palabras</p>
+                                    <div href="!#">Etiquetadas: {perfil.wordTagCount} </div>
+                                    <div href="!#">Subidas: {perfil.uploadWordCount} </div>
+                                </div>
+                                <div>
+                                    <p>Ahorcados</p>
+                                    <div href="!#">Completados: {perfil.hangmanTagCount} </div>
+                                    <div href="!#">Subidos: {perfil.uploadHangmanCount} </div>
+                                </div>
+                                <div>
+                                    <p>Selecciones Únicas</p>
+                                    <div href="!#">Completadas: {perfil.uniqueSelectionTagCount} </div>
+                                    <div href="!#">Subidas: {perfil.uploadUniqueSelectionCount} </div>
+                                </div>
                             </div>
                         </div>
                         <div className="col-md-8 tab-content profile-tab">
@@ -314,143 +322,3 @@ const Profile = ( props ) => {
 }
  
 export default Profile;
-
-/*
-<div className="padding">
-                    <div className="col container d-flex justify-content-center">
-                        <div className="col-xl-12 col-md-12">
-                            <div className="card user-card-full">
-                                <div className="row m-l-0 m-r-0">
-                                    <div className="col-sm-4 bg-c-lite-green user-profile">
-                                        <div className="card-block text-center text-white">
-                                            <div className="m-b-25 "> 
-                                                <img src={ usuario.urlFile ? usuario.urlFile : ProfileDefault } className="img-radius" alt="" /> 
-                                            </div>
-                                            <h4 className="f-w-600"> {usuario.firstname} </h4>
-                                            <h5 className="f-w-400"> {usuario.lastname} </h5>
-                                            <p className="f-w-200" > <i>
-                                                { usuario.isExpert
-                                                ?
-                                                    "Integrante de FireSES"
-                                                :
-                                                    "No pertenezco a FireSES"
-                                                }
-                                            </i> </p> 
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-8">
-                                        <div className="card-block">
-                                            <div className="row">
-                                                <div className="col-sm-6">
-                                                    <h6 className="m-b-20 p-b-5 b-b-default f-w-600"> Mi información de perfil de usuario </h6>
-                                                </div>
-                                                <div className="col-sm-6 col-log-12 col-xl-6">
-                                                            <button
-                                                                className={
-                                                                    perfil.league_id.league === "Bronce"
-                                                                    ?
-                                                                        "disableBtn"
-                                                                    :
-                                                                        "profile-edit-btn"
-                                                                }
-                                                                onClick={() => updateButtonAction() }
-                                                                disabled={
-                                                                    perfil.league_id.league === "Bronce"
-                                                                    ?
-                                                                        true
-                                                                    :
-                                                                        false
-                                                                }
-                                                            >
-                                                                <div className="row">
-                                                                    <div className="col-sm-6 col-log-12 col-xl-8">
-                                                                            Editar Perfil
-                                                                    </div>
-                                                                    <div className="col-sm-6 col-log-12 col-xl-4">
-                                                                        <FaEdit className="iconoEditar"/>
-                                                                    </div>
-                                                                </div>
-                                                            </button>
-                                                </div>
-                                            </div>
-                                            <div className="row">
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Email </p>
-                                                    <h6 className="text-muted f-w-400"> {usuario.email} </h6>
-                                                </div>
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Teléfono </p>
-                                                    <h6 className="text-muted f-w-400"> {usuario.phone} </h6>
-                                                </div>
-                                            </div>
-                                            <br/>
-                                            <div className="row">
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Edad </p>
-                                                    <h6 className="text-muted f-w-400"> {usuario.age} </h6>
-                                                </div>
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Género </p>
-                                                    <h6 className="text-muted f-w-400"> {usuario.gender} </h6>
-                                                </div>
-                                            </div>
-                                            <br/>
-                                            <div className="row">
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Apodo </p>
-                                                    <h6 className="text-muted f-w-400"> {perfil.nicknameUsed} </h6>
-                                                </div>
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Marco </p>
-                                                    <div className={`div-imageUser-profile ${perfil.frameUsedCss}`} >
-                                                        <img src={ usuario.urlFile ? usuario.urlFile : ProfileDefault } alt=""/>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600"> Perfil de juego en E-ncendio </h6>
-                                            <div className="row">
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Liga </p>
-                                                    <h6 className="text-muted f-w-400">
-                                                        {perfil.league_id.league} 
-                                                        <span className="badge badge-pill badge-light align-middle">
-                                                            <img src={
-                                                                    perfil.league_id.league === "Bronce" ? BronceBadge
-                                                                    : perfil.league_id.league === "Plata" ? PlataBadge
-                                                                    : perfil.league_id.league === "Oro" ? OroBadge : null
-                                                                }
-                                                                alt=""
-                                                                className="user-badge"
-                                                            />
-                                                        </span>
-                                                    </h6>
-                                                </div>
-                                                <div className="col-sm-6">
-                                                    <p className="m-b-10 f-w-600"> Puntuación </p>
-                                                    <h6 className="text-muted f-w-400"> {perfil.score} </h6>
-                                                </div>
-                                            </div>
-                                            <br/>
-                                            <div className="row">
-                                                <div className="col-sm-4">
-                                                    <p className="m-b-10 f-w-600"> Nivel etiquetando Imágenes </p>
-                                                    <h6 className="text-muted f-w-400"> {perfil.level_image_id.level} </h6>
-                                                </div>
-                                                <div className="col-sm-4">
-                                                    <p className="m-b-10 f-w-600"> Nivel etiquetando Palabras </p>
-                                                    <h6 className="text-muted f-w-400"> {perfil.level_word_id.level} </h6>
-                                                </div>
-                                                <div className="col-sm-4">
-                                                    <p className="m-b-10 f-w-600"> Nivel asignando 1 palabra a imágenes </p>
-                                                    <h6 className="text-muted f-w-400"> {perfil.level_four_image_id.level} </h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-*/
